be3420b1c34c4b21ba43c16f1d2f514adaa4c395
Muhammad Akhta <akhtacaesar@gmail.com>
2022-09-27 01:02:24 +0700

    Added Methods

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ const app = Vue.createApp({
             gender: 'male',
             picture: 'https://avatars.githubusercontent.com/u/66517802?v=4',
         }
-    }
+    },
+    methods: {
+        getUser() {
+            this.firstName = 'Anna',
+            this.lastName = 'Prochniak',
+            this.email = 'annaprochniak@gmail.com',
+            this.gender = 'female',
+            this.picture = 'https://starsbiopedia.com/wp-content/uploads/2022/06/Anna-Prochniak-Bio.jpg'
+        },
+    },
 })
 
 app.mount('#app')
