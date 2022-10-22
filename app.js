2b877d80d4d961620ab65c406172da0df93ffca7
Muhammad Akhta <akhtacaesar@gmail.com>
2022-10-22 17:01:34 +0700

    Added Most of The Invoice Function

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
+var grand_total = 0;
+
 const formatter = new Intl.NumberFormat('id-ID', {
     style: 'currency',
     currency: 'IDR',
   });
 
+function clear() {
+    document.getElementById("nama-produk").reset();
+    document.getElementById("jumlah-produk").reset();
+    document.getElementById("harga-produk").reset();
+}
+
 function tambahDetails() {
+    const theDetails = document.getElementById("list-details");
+
     var name = document.getElementById("name").value;
-    console.log(name);
-
     var date = document.getElementById("date").value;
-    console.log(date);
-
     var address = document.getElementById("address").value;
-    console.log(address);
-
     var payment = document.getElementById("payment").value;
-    console.log(payment);
+
+    var det_name = document.createElement("h6");
+    det_name.innerHTML = '<strong>Pembeli : </strong>' + name;
+    var det_date = document.createElement("h6");
+
+    det_date.innerHTML = '<strong>Tanggal Pembelian : </strong>' + date;
+    var det_payment = document.createElement("h6");
+    det_payment.innerHTML = '<strong>Metode Pembayaran : </strong>' + payment;
+    var det_address = document.createElement("h6");
+    det_address.innerHTML = '<strong>Alamat Pengiriman : </strong>' + address;
+    
+
+    theDetails.appendChild(det_name);
+    theDetails.appendChild(det_date);
+    theDetails.appendChild(det_payment);
+    theDetails.appendChild(det_address);
 }
 
 function tambahProduk() {
+    let listProduct = document.getElementById("list-produk");
+    let theTotal = document.getElementById("grand-total");
+
     var product_name = document.getElementById("nama-produk").value;
     var product_amount = document.getElementById("jumlah-produk").value;
     var product_price = document.getElementById("harga-produk").value;
@@ -34,16 +56,13 @@ function tambahProduk() {
     pro_price.innerHTML = formatter.format(product_price);
     pro_total.innerHTML = formatter.format(product_amount*product_price);
 
+    inner_total = product_amount*product_price;
+    grand_total += inner_total;
     theBody.appendChild(theRow);
     theRow.appendChild(pro_name);
     theRow.appendChild(pro_amount);
     theRow.appendChild(pro_price);
     theRow.appendChild(pro_total);
-    document.getElementById("list-produk").appendChild(theBody);
-}
-
-function clear() {
-    document.getElementById("nama-produk").value = NULL;
-    document.getElementById("jumlah-produk").value = NULL;
-    document.getElementById("harga-produk").value = NULL;
+    listProduct.appendChild(theBody);
+    theTotal.innerHTML = "<strong>Total Tagihan : </strong>" + formatter.format(grand_total);
 }
