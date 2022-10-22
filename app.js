9a8eef79d919f73703f17a216a0bcaa267cc51f2
Muhammad Akhta <akhtacaesar@gmail.com>
2022-10-22 12:57:48 +0700

    Major Revision of Forms
    Also Implementation of Client-Side Storage

---
+++ app.js
@@ -0,0 +1,14 @@
+
+function firstStage() {
+    var name = document.getElementById("name").value;
+    console.log(name);
+
+    var date = document.getElementById("date").value;
+    console.log(date);
+
+    var address = document.getElementById("address").value;
+    console.log(address);
+
+    var payment = document.getElementById("payment").value;
+    console.log(payment);
+}
