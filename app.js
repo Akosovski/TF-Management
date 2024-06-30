4bb03f31a6c91156883f4d097d491c8f57a16f4e
Muhammad Akhta <akhtacaesar@gmail.com>
2024-06-30 22:20:35 +0700

    Major Update to Discount

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var grand_total = 0;
+var grand_total_discount = 0;
 const formatter = new Intl.NumberFormat('id-ID', {
     style: 'currency',
     currency: 'IDR',
@@ -58,48 +59,79 @@ function tambahProduk() {
     var product_name = document.getElementById("nama-produk").value;
     var product_amount = document.getElementById("jumlah-produk").value;
     var product_price = document.getElementById("harga-produk").value;
-    var product_discount = document.getElementById("diskon-produk").value;
 
     var theBody = document.createElement("TBODY");
     var theRow = document.createElement("TR");
     var pro_name = document.createElement("TD");
     var pro_amount = document.createElement("TD");
     var pro_price = document.createElement("TD");
-    var pro_discount = document.createElement("TD");
     var pro_total = document.createElement("TD");
 
     pro_name.innerHTML = product_name;
     pro_amount.innerHTML = product_amount;
     pro_price.innerHTML = formatter.format(product_price);
-    pro_discount.innerHTML = formatter.format(product_discount);
-    pro_total.innerHTML = formatter.format((product_amount*product_price)-product_discount);
+    pro_total.innerHTML = formatter.format(product_amount*product_price);
 
-    inner_total = (product_amount*product_price)-product_discount;
+    inner_total = product_amount*product_price;
 
     grand_total += inner_total;
     theBody.appendChild(theRow);
     theRow.appendChild(pro_name);
     theRow.appendChild(pro_amount);
     theRow.appendChild(pro_price);
-    theRow.appendChild(pro_discount);
     theRow.appendChild(pro_total);
     listProduct.appendChild(theBody);
-
-    if(product_discount > 0){
-        theTotal.innerHTML = "<strong>Sebelum Diskon : </strong><s>" + formatter.format(grand_total) + 
-        "</s> <br><strong>Diskon Sebesar : </strong><u>" + formatter.format(product_discount) + 
-        "</u><br><br><strong>Total Tagihan : </strong>" + formatter.format(grand_total - product_discount);
-    }
 
     theTotal.innerHTML = "<strong>Total Tagihan : </strong>" + formatter.format(grand_total);
 }
 
-function tambahDiskon() {
+function tambahDiskonProduk(){
+    let listProduct = document.getElementById("list-produk");
     let theTotal = document.getElementById("grand-total");
-    var diskon = document.getElementById("jumlah-diskon").value;
-    if(diskon > 0){
+
+    var product_name = document.getElementById("nama-produk").value;
+    var product_discount_amount = document.getElementById("jumlah-diskon").value;
+    var product_discount = document.getElementById("diskon-produk").value;
+
+    var theBody = document.createElement("TBODY");
+    var theRow = document.createElement("TR");
+    var disc_name = document.createElement("TD");
+    var disc_amount = document.createElement("TD");
+    var disc_price = document.createElement("TD");
+    var disc_total = document.createElement("TD");
+
+    disc_name.innerHTML = "Diskon " + product_name;
+    disc_amount.innerHTML = product_discount_amount;
+    disc_price.innerHTML = formatter.format(product_discount);
+    disc_total.innerHTML = formatter.format(product_discount_amount*product_discount);
+
+    theBody.appendChild(theRow);
+    theRow.appendChild(disc_name);
+    theRow.appendChild(disc_amount);
+    theRow.appendChild(disc_price);
+    theRow.appendChild(disc_total);
+    listProduct.appendChild(theBody);
+
+    discounted_total = product_discount_amount*product_discount;
+    grand_total_discount += discounted_total
+
+    theTotal.innerHTML = "<strong>Sebelum Diskon : </strong><s>" + formatter.format(grand_total) + 
+    "</s> <br><strong>Diskon Sebesar : </strong><u>" + formatter.format(grand_total_discount) + 
+    "</u><br><br><strong>Total Tagihan : </strong>" + formatter.format(grand_total - grand_total_discount);
+    grand_total -= grand_total_discount
+}
+
+function tambahDP() {
+    let theTotal = document.getElementById("grand-total");
+    var dp = document.getElementById("jumlah-dp").value;
+    if(grand_total_discount > 0 && dp > 0){
         theTotal.innerHTML = "<strong>Sebelum Diskon : </strong><s>" + formatter.format(grand_total) + 
-        "</s> <br><strong>Diskon Sebesar : </strong><u>" + formatter.format(diskon) + 
-        "</u><br><br><strong>Total Tagihan : </strong>" + formatter.format(grand_total - diskon);
+        "</s> <br><strong>Diskon Sebesar : </strong>" + formatter.format(grand_total_discount) + 
+        "<br><u><strong>Uang Muka (DP) : </strong>" + formatter.format(dp) + 
+        "</u><br><br><strong>Total Tagihan : </strong>" + formatter.format(grand_total - grand_total_discount - dp);
+    }
+    else if(grand_total_discount == 0 && dp > 0){
+        theTotal.innerHTML = "<strong>Uang Muka (DP) : </strong>" + formatter.format(dp) + 
+        "</u><br><br><strong>Total Tagihan : </strong>" + formatter.format(grand_total - dp);
     }
 }
